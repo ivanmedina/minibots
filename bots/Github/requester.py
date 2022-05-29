7b668492ac1252c2b1e49462a853151d9647b3a5
Ivan Medina <ivansk8.imm@gmail.com>
2022-05-28 21:56:44 -0500

    feat: github bot requester

---
+++ bots/Github/requester.py
@@ -0,0 +1,26 @@
+from requests import request, Response
+from bots.Github import common
+
+class requestsGithub():
+    
+    def __init__( self, repositorio, resource ):
+        self.repositorio = repositorio
+        self.resource = resource
+
+    def requester( self, url )->Response:
+        bconection = self.repositorio.bconnect( self.resource )
+        if bconection.status_code==200:
+            self.repositorio.disconnect( bconection )
+            bconection=self.repositorio.get( url )
+            self.repositorio.disconnect( bconection )
+        return bconection
+
+    def requestUser( self, username )->Response:
+        return self.requester(f'{self.resource}/users/{username}' )
+
+    def requestFollows( self, username, param )->Response:
+        return self.requester( f'{self.resource}/users/{username}/{param}?per_page=100' )
+
+    def requestUserRepos( self, username )->Response:
+        return self.requester( f'{self.resource}/users/{username}/repos' )
+
