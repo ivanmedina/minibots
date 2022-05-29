6b8be6f50f87e2b49bec81c7574246541767d2d2
Ivan Medina <ivansk8.imm@gmail.com>
2022-05-28 21:34:11 -0500

    feat: github respository class

---
+++ bots/Github/repository.py
@@ -0,0 +1,22 @@
+import common
+from bot.entity import entity
+import json
+
+class repository(entity):
+    
+    name: str
+    description: str
+    url: str
+    size: int
+    clone: str
+
+    def __init__( self, id:int, name:str, description:str, url:str, size:int, clone:str ):
+        self.id=id
+        self.name=name
+        self.description=description
+        self.url=url
+        self.size=size
+        self.clone=clone
+    
+    def __str__(self) -> str:
+        return json.dumps(self.__dict__, indent=2, separators=(',', ': '))
